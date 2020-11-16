e20bab03edd3651b957616547754ea21a07c0cd7
jaspreetjhass <jaspreetjhass91@gmail.com>
2020-11-17 02:35:37 +0530

    Update TravelDetailClient.java

--- src/main/java/org/jp/passenger/travel/batch/feign/TravelDetailClient.java
+++ src/main/java/org/jp/passenger/travel/batch/feign/TravelDetailClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "TravelDetailClient", url = "http://localhost:8070/passengerCore")
+@FeignClient(name = "TravelDetailClient", url = "${PASSENGER_CORE_URL}")
 public interface TravelDetailClient {
 
 	@GetMapping("/{passengerId}")
